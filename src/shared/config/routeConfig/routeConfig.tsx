d874e583a72de8953e4cf28d3c39ef055c159e6d
MikhailDnd <dundukovmi@gmail.com>
2025-08-12 18:13:49 +0500

    add router config, move router to shared folder

---
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -0,0 +1,23 @@
+import {RouteProps} from "react-router-dom"
+import {MainPage} from "pages/MainPages/MainPage";
+import {AboutPage} from "pages/MainPages/AboutPage";
+
+export enum AppRoutes {
+    MAIN = 'main',
+    ABOUT = 'about',
+}
+
+export const RoutePath: Record<AppRoutes, string> = {
+    [AppRoutes.MAIN]: '/',
+    [AppRoutes.ABOUT]: '/about',
+}
+
+export const routeConfig: Record<AppRoutes, RouteProps> = {
+    [AppRoutes.MAIN]: {
+        path: RoutePath.main,
+        element: <MainPage/>
+    }, [AppRoutes.ABOUT]: {
+        path: RoutePath.about,
+        element: <AboutPage/>
+    },
+}
