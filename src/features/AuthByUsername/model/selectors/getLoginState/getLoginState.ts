6e48509181343fdba8419144ed8cd982cc9d3db6
MikhailDnd <dundukovmi@gmail.com>
2025-09-05 23:55:10 +0500

    add tests for login module

--- src/features/AuthByUsername/model/selectors/getLoginState/getLoginState.ts
+++
@@ -1,4 +0,0 @@
-import { StateSchema } from 'app/providers/StoreProvider';
-
-// селекторы лучше максимально разбивать, делать для логина, пароля, ошибки и тд отдельно, чтобы избегать перерисовок
-export const getLoginState = (state: StateSchema) => state?.loginForm;
