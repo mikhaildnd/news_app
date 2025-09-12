9bd77f40242e3c8637e63709bd2891b6c8d3dd37
MikhailDnd <dundukovmi@gmail.com>
2025-09-12 21:18:26 +0500

    took buildBabelLoader.ts outside

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,25 +6,31 @@ import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
-    paths, isDev, apiUrl, project,
+    paths,
+    isDev,
+    apiUrl,
+    project,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-    const plugins = [
+    const plugins: webpack.WebpackPluginInstance[] = [
         new HTMLWebpackPlugin({
             template: paths.html,
         }),
         new webpack.ProgressPlugin(),
-        // MiniCssExtractPlugin лучше подключать только в prod
-        !isDev
-        && new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
             __API__: JSON.stringify(apiUrl),
             __PROJECT__: JSON.stringify(project),
         }),
-    ].filter(Boolean) as webpack.WebpackPluginInstance[]; // убираем false/undefined;
+    ];
+
+    if (!isDev) {
+        plugins.push(
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css',
+            }),
+        );
+    }
 
     if (isDev) {
         plugins.push(
