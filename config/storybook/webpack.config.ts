2750b3bbef92504c0d60b2693f47db4749508302
MikhailDnd <dundukovmi@gmail.com>
2025-09-12 16:53:06 +0500

    fix screenshot tests

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -3,37 +3,47 @@ import path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
 
-export default ({ config }: {config: webpack.Configuration}) => {
+export default ({ config }: { config: webpack.Configuration }) => {
     const paths: BuildPaths = {
         build: '',
         html: '',
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
+
     config!.resolve!.modules!.push(paths.src);
     config!.resolve!.extensions!.push('.ts', '.tsx');
 
-    // eslint-disable-next-line no-param-reassign
-    config!.module!.rules = (config!.module!.rules as webpack.RuleSetRule[]).map((rule) => {
+    // фикс: всегда используем относительные пути для ассетов
+    config!.output = {
+        ...config!.output,
+        publicPath: '', // вместо '/static/' → относительные урлы
+    };
+
+    // убираем svg из дефолтных правил
+    config!.module!.rules = (config!.module!.rules as RuleSetRule[]).map((rule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
         }
-
         return rule;
     });
 
+    // наш loader для svg
     config!.module!.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     });
 
+    // css loader
     config!.module!.rules.push(buildCssLoader(true));
 
-    config!.plugins!.push(new DefinePlugin({
-        __IS_DEV__: true,
-        __API__: JSON.stringify(''),
-        __PROJECT__: JSON.stringify('storybook'),
-    }));
+    config!.plugins!.push(
+        new DefinePlugin({
+            __IS_DEV__: true,
+            __API__: JSON.stringify(''),
+            __PROJECT__: JSON.stringify('storybook'),
+        }),
+    );
 
     return config;
 };
