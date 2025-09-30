c438ac707fdf1cf20d9f4bf6b68938b175f5c6c0
MikhailDnd <dundukovmi@gmail.com>
2025-10-01 02:59:25 +0500

    update articlesPageSlice.ts.ts, add new flags: page, hasMore

--- src/pages/ArticlesPage/model/slice/articlesPageSlice.ts
+++ src/pages/ArticlesPage/model/slice/articlesPageSlice.ts
@@ -7,7 +7,7 @@ import {
 import type { ArticlesPageSchema } from '../types/articlesPageSchema';
 import { Article, ArticleView } from 'entities/Article';
 import { rootReducer } from 'app/providers/StoreProvider';
-import { fetchArticlesList } from '../../model/services/fetchArticlesList';
+import { fetchArticlesList } from '../services/fetchArticlesList/fetchArticlesList';
 import { ARTICLE_VIEW_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 const articlesAdapter = createEntityAdapter<Article>();
@@ -20,16 +20,23 @@ export const articlesPageSlice = createSlice({
         ids: [],
         entities: {},
         view: ArticleView.SMALL,
+        page: 1,
+        hasMore: true,
     }),
     reducers: {
         setView: (state, action: PayloadAction<ArticleView>) => {
             state.view = action.payload;
             localStorage.setItem(ARTICLE_VIEW_LOCALSTORAGE_KEY, action.payload);
         },
+        setPage: (state, action: PayloadAction<number>) => {
+            state.page = action.payload;
+        },
         initState: (state) => {
-            state.view = localStorage.getItem(
+            const view = localStorage.getItem(
                 ARTICLE_VIEW_LOCALSTORAGE_KEY,
             ) as ArticleView; // local storage хранит только строки - кастуем в енам
+            state.view = view;
+            state.limit = view === ArticleView.BIG ? 4 : 9;
         },
     },
     extraReducers: (builder) => {
@@ -42,7 +49,8 @@ export const articlesPageSlice = createSlice({
                 fetchArticlesList.fulfilled,
                 (state, action: PayloadAction<Article[]>) => {
                     state.isLoading = false;
-                    articlesAdapter.setAll(state, action.payload);
+                    articlesAdapter.addMany(state, action.payload);
+                    state.hasMore = action.payload.length > 0;
                 },
             )
             .addCase(fetchArticlesList.rejected, (state, action) => {
