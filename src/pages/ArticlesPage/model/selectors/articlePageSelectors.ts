57a70f0527043c892a15687c499022307f0107f5
MikhailDnd <dundukovmi@gmail.com>
2025-10-01 03:00:09 +0500

    add articlePageSelectors.ts selectors

--- src/pages/ArticlesPage/model/selectors/articlePageSelectors.ts
+++ src/pages/ArticlesPage/model/selectors/articlePageSelectors.ts
@@ -7,3 +7,9 @@ export const getArticlesPageError = (state: RootState) =>
     state.articlesPage?.error;
 export const getArticlesPageView = (state: RootState) =>
     state.articlesPage?.view || ArticleView.SMALL;
+export const getArticlesPageNum = (state: RootState) =>
+    state.articlesPage?.page || 1;
+export const getArticlesPageLimit = (state: RootState) =>
+    state.articlesPage?.limit || 9;
+export const getArticlesPageHasMore = (state: RootState) =>
+    state.articlesPage?.hasMore;
