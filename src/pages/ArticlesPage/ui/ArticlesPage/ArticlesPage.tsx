b20a3c55e5ce4b86593244412f2223b48787d1d9
MikhailDnd <dundukovmi@gmail.com>
2025-09-30 19:24:49 +0500

    add articlePage component, styles, stories

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,6 +1,24 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { memo } from 'react';
+import { memo, useCallback } from 'react';
+import { useSelector } from 'react-redux';
 import cls from './ArticlesPage.module.scss';
+import {
+    ArticleList,
+    ArticleView,
+    ArticleViewSelector,
+} from 'entities/Article';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { fetchArticlesList } from '../../model/services/fetchArticlesList';
+import {
+    articlesPageSliceActions,
+    articlesPageSliceSelectors,
+} from '../../model/slice/articlesPageSlice';
+import {
+    getArticlesPageError,
+    getArticlesPageIsLoading,
+    getArticlesPageView,
+} from '../../model/selectors/articlePageSelectors';
 
 interface ArticlesPageProps {
     className?: string;
@@ -8,9 +26,33 @@ interface ArticlesPageProps {
 
 const ArticlesPage = (props: ArticlesPageProps) => {
     const { className } = props;
+    const dispatch = useAppDispatch();
+    const articles = useSelector(articlesPageSliceSelectors.selectAll);
+    const isLoading = useSelector(getArticlesPageIsLoading);
+    const error = useSelector(getArticlesPageError);
+    const view = useSelector(getArticlesPageView);
+
+    const onChangeView = useCallback(
+        (newView: ArticleView) => {
+            dispatch(articlesPageSliceActions.setView(newView));
+        },
+        [dispatch],
+    );
+
+    useInitialEffect(() => {
+        void dispatch(fetchArticlesList());
+        dispatch(articlesPageSliceActions.initState());
+    });
 
     return (
-        <div className={classNames(cls.ArticlesPage, {}, [className])}></div>
+        <div className={classNames(cls.ArticlesPage, {}, [className])}>
+            <ArticleViewSelector view={view} onViewClick={onChangeView} />
+            <ArticleList
+                isLoading={isLoading}
+                view={view}
+                articles={articles}
+            />
+        </div>
     );
 };
 
