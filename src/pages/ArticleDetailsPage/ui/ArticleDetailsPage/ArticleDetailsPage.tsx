09cfaad9f13a71d427c6ed59eebda68598fc3cd2
MikhailDnd <dundukovmi@gmail.com>
2025-09-30 19:32:25 +0500

    add 'return' button

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -2,7 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo, useCallback } from 'react';
 import { ArticleDetails } from 'entities/Article';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Text } from 'shared/ui/Text/Text';
 import { CommentList } from 'entities/Comment';
 import { useSelector } from 'react-redux';
@@ -14,6 +14,8 @@ import { getArticleCommentsIsLoading } from '../../model/selectors/comments';
 import { fetchCommentsByArticleId } from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
 import AddCommentForm from 'features/AddCommentForm/ui/addCommentForm/addCommentForm';
 import { addCommentForArticle } from '../../model/services/addCommentForArticle/addCommentForArticle';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 
 interface ArticleDetailsPageProps {
     className?: string;
@@ -22,11 +24,16 @@ interface ArticleDetailsPageProps {
 const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     const { className } = props;
     const { t } = useTranslation('article-details');
+    const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const { id } = useParams<{ id: string }>();
     const comments = useSelector(articleDetailsCommentsSelectors.selectAll);
     const commentsIsLoading = useSelector(getArticleCommentsIsLoading);
     // const error = useSelector(getArticleDetailsError);
+
+    const onBackToList = useCallback(() => {
+        void navigate(RoutePath.articles);
+    }, [navigate]);
 
     const onSendComment = useCallback(
         (text: string) => {
@@ -52,6 +59,9 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     return (
         // <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
         <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+            <Button theme={ButtonTheme.OUTLINE} onClick={onBackToList}>
+                {t('Назад к списку')}
+            </Button>
             <ArticleDetails id={id} />
             <Text className={cls.commentTitle} title={t('Комментарии')} />
             <AddCommentForm onSendComment={onSendComment} />
