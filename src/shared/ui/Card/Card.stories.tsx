86cf5ed373cc74c3425923e6227da7769ad3b08f
MikhailDnd <dundukovmi@gmail.com>
2025-09-30 19:19:33 +0500

    add card component, styles, stories

---
+++ src/shared/ui/Card/Card.stories.tsx
@@ -0,0 +1,17 @@
+import { Card } from './Card';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Text } from 'shared/ui/Text/Text';
+
+const meta: Meta<typeof Card> = {
+    title: 'shared/Card',
+    component: Card,
+};
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        children: <Text title={'Title'} text={'Text'} />,
+    },
+};
