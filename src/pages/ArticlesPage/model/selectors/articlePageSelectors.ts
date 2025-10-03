38ef9dfaac625d400b322c38198e57124e426d74
MikhailDnd <dundukovmi@gmail.com>
2025-10-03 19:05:26 +0500

    add new selectors for sort

--- src/pages/ArticlesPage/model/selectors/articlePageSelectors.ts
+++ src/pages/ArticlesPage/model/selectors/articlePageSelectors.ts
@@ -1,5 +1,5 @@
 import { RootState } from 'app/providers/StoreProvider/config/store';
-import { ArticleView } from 'entities/Article';
+import { ArticleSortField, ArticleType, ArticleView } from 'entities/Article';
 
 export const getArticlesPageIsLoading = (state: RootState) =>
     state.articlesPage?.isLoading || false;
@@ -15,3 +15,11 @@ export const getArticlesPageHasMore = (state: RootState) =>
     state.articlesPage?.hasMore;
 export const getArticlesPageInited = (state: RootState) =>
     state.articlesPage?._inited;
+export const getArticlesPageOrder = (state: RootState) =>
+    state.articlesPage?.order ?? 'asc';
+export const getArticlesPageSort = (state: RootState) =>
+    state.articlesPage?.sort ?? ArticleSortField.CREATED;
+export const getArticlesPageSearch = (state: RootState) =>
+    state.articlesPage?.search ?? '';
+export const getArticlesPageType = (state: RootState) =>
+    state.articlesPage?.type ?? ArticleType.ALL;
