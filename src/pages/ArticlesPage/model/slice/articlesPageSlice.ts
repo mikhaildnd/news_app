8b04d83263001426984f2cf8989726c53d441700
MikhailDnd <dundukovmi@gmail.com>
2025-10-03 18:41:02 +0500

    upd article slice, add new state fields and new actions for sort

--- src/pages/ArticlesPage/model/slice/articlesPageSlice.ts
+++ src/pages/ArticlesPage/model/slice/articlesPageSlice.ts
@@ -5,10 +5,16 @@ import {
     type WithSlice,
 } from '@reduxjs/toolkit';
 import type { ArticlesPageSchema } from '../types/articlesPageSchema';
-import { Article, ArticleView } from 'entities/Article';
+import {
+    Article,
+    ArticleType,
+    ArticleView,
+    ArticleSortField,
+} from 'entities/Article';
 import { rootReducer } from 'app/providers/StoreProvider';
 import { fetchArticlesList } from '../services/fetchArticlesList/fetchArticlesList';
 import { ARTICLE_VIEW_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+import { SortOrder } from 'shared/types';
 
 const articlesAdapter = createEntityAdapter<Article>();
 
@@ -23,6 +29,11 @@ export const articlesPageSlice = createSlice({
         page: 1,
         hasMore: true,
         _inited: false,
+        limit: 9,
+        sort: ArticleSortField.CREATED,
+        search: '',
+        order: 'asc',
+        type: ArticleType.ALL,
     }),
     reducers: {
         setView: (state, action: PayloadAction<ArticleView>) => {
@@ -31,6 +42,18 @@ export const articlesPageSlice = createSlice({
         },
         setPage: (state, action: PayloadAction<number>) => {
             state.page = action.payload;
+        },
+        setOrder: (state, action: PayloadAction<SortOrder>) => {
+            state.order = action.payload;
+        },
+        setSort: (state, action: PayloadAction<ArticleSortField>) => {
+            state.sort = action.payload;
+        },
+        setSearch: (state, action: PayloadAction<string>) => {
+            state.search = action.payload;
+        },
+        setType: (state, action: PayloadAction<ArticleType>) => {
+            state.type = action.payload;
         },
         initState: (state) => {
             const view =
@@ -44,18 +67,24 @@ export const articlesPageSlice = createSlice({
     },
     extraReducers: (builder) => {
         builder
-            .addCase(fetchArticlesList.pending, (state) => {
+            .addCase(fetchArticlesList.pending, (state, action) => {
                 state.error = undefined;
                 state.isLoading = true;
+
+                if (action.meta.arg.replace) {
+                    articlesAdapter.removeAll(state);
+                }
             })
-            .addCase(
-                fetchArticlesList.fulfilled,
-                (state, action: PayloadAction<Article[]>) => {
-                    state.isLoading = false;
+            .addCase(fetchArticlesList.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.hasMore = action.payload.length >= state.limit;
+
+                if (action.meta.arg.replace) {
+                    articlesAdapter.setAll(state, action.payload);
+                } else {
                     articlesAdapter.addMany(state, action.payload);
-                    state.hasMore = action.payload.length > 0;
-                },
-            )
+                }
+            })
             .addCase(fetchArticlesList.rejected, (state, action) => {
                 state.isLoading = false;
                 state.error = action.payload;
