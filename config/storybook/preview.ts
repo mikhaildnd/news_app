5937e868b4cfd2660e7ce2392b448d8facab2dc0
MikhailDnd <dundukovmi@gmail.com>
2025-10-21 16:59:18 +0500

    add msw, add api mock for storybook, upd screenshots

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -5,6 +5,9 @@ import { Theme } from '../../src/app/providers/ThemeProvider';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 import '../../src/app/styles/index.scss';
 import { StoreDecorator } from '../../src/shared/config/storybook/StoreDecorator/StoreDecorator';
+import { initialize, mswLoader } from 'msw-storybook-addon';
+
+initialize();
 
 const preview: Preview = {
     parameters: {
@@ -16,6 +19,8 @@ const preview: Preview = {
             },
         },
     },
+    // Provide the MSW addon loader globally
+    loaders: [mswLoader],
     decorators: [
         RouterDecorator,
         StyleDecorator,
--- src/pages/MainPage/ui/MainPage.stories.tsx
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import MainPage from './MainPage';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
 
 const meta: Meta<typeof MainPage> = {
     title: 'pages/MainPage',
@@ -15,4 +17,5 @@ export const Normal: Story = {
 
 export const Dark: Story = {
     args: {},
+    decorators: [ThemeDecorator(Theme.DARK)],
 };
