e0a9a7d05795608029425538997effb72eead2bb
MikhailDnd <dundukovmi@gmail.com>
2025-10-30 15:57:45 +0500

    add gesture, add react spring, add dynamic lib loading with AnimationProvider

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,52 +1,116 @@
-import { memo, ReactNode } from 'react';
 import { classNames, Mods } from 'shared/lib/classNames/classNames';
-import cls from './Drawer.module.scss';
+import React, { memo, ReactNode, useCallback, useEffect } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { Portal } from '../Portal/Portal';
+import { useAnimationLibs } from 'shared/lib/components/AnimationProvider';
 import { Overlay } from '../Overlay/Overlay';
-import { useModal } from 'shared/lib/hooks/useModal/useModal';
+import cls from './Drawer.module.scss';
+import { Portal } from '../Portal/Portal';
 
 interface DrawerProps {
     className?: string;
-    children?: ReactNode;
+    children: ReactNode;
     isOpen?: boolean;
     onClose?: () => void;
     lazy?: boolean;
 }
 
-export const Drawer = memo((props: DrawerProps) => {
-    const { className, children, isOpen, onClose, lazy } = props;
+const height = window.innerHeight - 100;
+
+export const DrawerContent = memo((props: DrawerProps) => {
+    const { Spring, Gesture } = useAnimationLibs();
+
+    const [{ y }, api] = Spring.useSpring(() => ({ y: height }));
     const { theme } = useTheme();
+    const { className, children, onClose, isOpen, lazy } = props;
 
-    const { isClosing, close, isMounted } = useModal({
-        animationDelay: 300,
-        isOpen,
-        onClose,
-    });
+    const openDrawer = useCallback(() => {
+        void api.start({ y: 0, immediate: false });
+    }, [api]);
 
-    const mods: Mods = {
-        [cls.opened]: isOpen,
-        [cls.isClosing]: isClosing,
+    useEffect(() => {
+        if (isOpen) {
+            openDrawer();
+        }
+    }, [api, isOpen, openDrawer]);
+
+    const close = (velocity = 0) => {
+        void api.start({
+            y: height,
+            immediate: false,
+            config: { ...Spring.config.stiff, velocity },
+            onResolve: onClose,
+        });
     };
 
-    if (lazy && !isMounted) {
+    const bind = Gesture.useDrag(
+        ({
+            last,
+            velocity: [, vy],
+            direction: [, dy],
+            movement: [, my],
+            cancel,
+        }) => {
+            if (my < -70) cancel();
+
+            if (last) {
+                if (my > height * 0.5 || (vy > 0.5 && dy > 0)) {
+                    close();
+                } else {
+                    openDrawer();
+                }
+            } else {
+                void api.start({ y: my, immediate: true });
+            }
+        },
+        {
+            from: () => [0, y.get()],
+            filterTaps: true,
+            bounds: { top: 0 },
+            rubberband: true,
+        },
+    );
+
+    if (!isOpen) {
         return null;
     }
+
+    const display = y.to((py) => (py < height ? 'block' : 'none'));
 
     return (
         <Portal>
             <div
-                className={classNames(cls.Drawer, mods, [
+                className={classNames(cls.Drawer, {}, [
                     className,
                     theme,
                     'app_drawer',
                 ])}
             >
                 <Overlay onClick={close} />
-                <div className={cls.content}>{children}</div>
+                <Spring.a.div
+                    className={cls.sheet}
+                    style={{
+                        display,
+                        bottom: `calc(-100vh + ${height - 100}px)`,
+                        y,
+                    }}
+                    {...bind()}
+                >
+                    {children}
+                </Spring.a.div>
             </div>
         </Portal>
     );
 });
 
+export const Drawer = memo((props: DrawerProps) => {
+    const { isLoaded } = useAnimationLibs();
+
+    if (!isLoaded) {
+        return null;
+    }
+
+    return <DrawerContent {...props} />;
+});
+
+DrawerContent.displayName = 'DrawerContent';
 Drawer.displayName = 'Drawer';
