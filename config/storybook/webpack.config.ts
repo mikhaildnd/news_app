3a415b118590624596f4efd85688e5bf70d5cfa6
MikhailDnd <dundukovmi@gmail.com>
2025-11-04 01:32:35 +0500

    upd webpack config for storybook, add alias

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -16,6 +16,10 @@ export default ({ config }: { config: Configuration }) => {
     // config!.resolve!.modules!.push(paths.src);
     config.resolve!.modules = [paths.src, 'node_modules'];
     config.resolve!.extensions!.push('.ts', '.tsx');
+    config.resolve!.alias = {
+        ...config.resolve!.alias,
+        '@': paths.src,
+    };
 
     // убираем обработку svg из дефолтных правил
     config.module!.rules = (config.module!.rules as RuleSetRule[]).map(
