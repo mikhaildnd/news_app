04f9e5eedd7e6530d588976e76c47e416449895c
MikhailDnd <dundukovmi@gmail.com>
2025-11-07 11:22:05 +0500

    add helpers for slices, selectors, actions

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,17 @@
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { Page } from '@/widgets/Page';
+import { Counter } from '@/entities/Counter';
 
 const MainPage = memo(function MainPage() {
     const { t } = useTranslation();
 
-    return <Page>{t('Главная страница')}</Page>;
+    return (
+        <Page>
+            <Counter />
+            {t('Главная страница')}
+        </Page>
+    );
 });
 
 export default MainPage;
