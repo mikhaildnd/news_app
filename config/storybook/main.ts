b0740849e867a1a5cc3962e553b2f8e108daae43
MikhailDnd <dundukovmi@gmail.com>
2025-11-18 16:32:42 +0500

    delete commented code from storybook config

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -18,18 +18,3 @@ const config: StorybookConfig = {
     },
 };
 export default config;
-
-// module.exports = {
-//     stories: [
-//         '../../src/**/*.stories.@(js|jsx|ts|tsx)',
-//     ],
-//     addons: [
-//         '@storybook/addon-links',
-//         '@storybook/addon-essentials',
-//         '@storybook/addon-interactions',
-//     ],
-//     framework: '@storybook/react',
-//     core: {
-//         builder: 'webpack5',
-//     },
-// };
